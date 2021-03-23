e0fcf855b7ec5d533fb2ba4a6791e2fb298babd8
Fortis <fortispotens@gmail.com>
2021-03-23 20:25:24 +0000

    setup Add task UI and functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState} from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   
@@ -25,6 +26,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = { id, ...task, }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     console.log('delete', id)
@@ -33,12 +41,13 @@ function App() {
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ... task, reminder: !task.reminder } : task))
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to show' }
     </div>
   );
